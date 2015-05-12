1472a553700d4bf684585a02d8f79a9608a5c2cf
Ryan Brady <ryanbradynd05@gmail.com>
2015-05-12 00:30:08 -0400

    Working editor output
    Uses eval for now, can be fixed later

--- app/components/editor/editor-directive.js
+++ app/components/editor/editor-directive.js
@@ -2,11 +2,36 @@
 
 angular.module('jslab.components')
 .directive('jslabEditor', [function() {
+  var controller = ['$scope', '$log', function($scope, $log) {
+    $scope.aceLoaded = function(editor) {
+      $log.debug('aceLoaded', editor, $scope);
+      editor.$blockScrolling = Infinity;
+    };
+    $scope.aceChanged = function(e) {
+      var editor = e[1];
+      $log.debug('aceChanged', e);
+      var values = editor.getValue().split('\n');
+      var results = [];
+      angular.forEach(values, function(value) {
+        var result = '';
+        try {
+          // TODO: Find a way without using eval
+          result = eval(value); // jshint ignore:line
+        } catch (e) {
+          $log.error(e);
+        }
+        results.push(result);
+      });
+      $scope.results = results;
+    };
+  }];
+
   return {
       restrict: 'E',
       scope: {
         code: '='
       },
-      templateUrl: 'components/editor/editor.html'
+      templateUrl: 'components/editor/editor.html',
+      controller: controller
     };
 }]);
