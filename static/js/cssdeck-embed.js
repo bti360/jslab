9ba3a4c6283994626c21128bbb0583aa4fceb65e
Ryan Brady <ryanbradynd05@gmail.com>
2015-06-09 23:45:27 -0400

    Update gotchas repl

--- static/js/cssdeck-embed.js
+++ static/js/cssdeck-embed.js
@@ -5,7 +5,7 @@
         host: "http://cssdeck.com",
         url_slug: "",
         version: 0,
-        username: "",
+        username: "bti360",
         height: 95,
         pane: "output",
         buildUrl: function() {
@@ -16,26 +16,26 @@
             var t;
             return t = document.createElement("iframe"), t["class"] = "_cssdeck_embed", t.src = e, t.frameborder = "0", t.style.border = "1px solid #3c404b", t.style.width = "101%", t.style.minHeight = this.height + "vh", t;
         }
-    }, 
-    r = document.querySelectorAll("._cssdeck_embed"), 
+    },
+    r = document.querySelectorAll("._cssdeck_embed"),
     t = r.length,
     o = 0;
     while (o < t) {
-        n = r[o], 
-        l = n.getAttribute("data-href"), 
-        h = n.getAttribute("data-version"), 
-        c = n.getAttribute("data-username"), 
-        f = n.getAttribute("data-pane"), 
-        s = n.getAttribute("data-height"), 
-        i = Object.create(e), 
-        l && (i.url_slug = l), 
-        h && (i.version = h), 
-        c && (i.username = c), 
-        f && (i.pane = f), 
-        s && (i.height = s), 
-        a = i.buildUrl(), 
-        u = i.buildIframeNode(a), 
-        n.parentNode.replaceChild(u, n), 
+        n = r[o],
+        l = n.getAttribute("data-href"),
+        h = n.getAttribute("data-version"),
+        c = n.getAttribute("data-username"),
+        f = n.getAttribute("data-pane"),
+        s = n.getAttribute("data-height"),
+        i = Object.create(e),
+        l && (i.url_slug = l),
+        h && (i.version = h),
+        c && (i.username = c),
+        f && (i.pane = f),
+        s && (i.height = s),
+        a = i.buildUrl(),
+        u = i.buildIframeNode(a),
+        n.parentNode.replaceChild(u, n),
         ++o;
     }
 }).call(this);
